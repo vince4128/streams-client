cd1a3ef822983e2e4af869564c48917d5dda4d9a
vince4128 <vrlt.vincent@gmail.com>
2019-04-03 12:56:28 +0200

    base routes added

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+
 
 const App = () => {
-    return <div>App</div>;
+    return <div>
+        <BrowserRouter>
+           <div>
+               <Route path="/" exact component={StreamList}></Route>
+               <Route path="/streams/new" exact component={StreamCreate}></Route>
+               <Route path="/streams/edit" exact component={StreamEdit}></Route>
+               <Route path="/streams/delete" exact component={StreamDelete}></Route>
+               <Route path="/streams/show" exact component={StreamShow}></Route>               
+           </div>
+        </BrowserRouter>
+    </div>;
 };
 
 export default App;
