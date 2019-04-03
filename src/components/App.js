8be95d05fdcfe1e1acf5e696f7a23a7d1f21bf3f
vince4128 <vrlt.vincent@gmail.com>
2019-04-03 13:08:43 +0200

    import cleaned

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 
 const App = () => {
-    return <div>
-        <BrowserRouter>
+    return <div>        
+        <BrowserRouter>            
            <div>
+            <Header/>
                <Route path="/" exact component={StreamList}></Route>
                <Route path="/streams/new" exact component={StreamCreate}></Route>
                <Route path="/streams/edit" exact component={StreamEdit}></Route>
