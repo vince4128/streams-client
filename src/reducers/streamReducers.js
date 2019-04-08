e685a4580f4fc5976937428154dc1965c551ba70
vince4128 <vrlt.vincent@gmail.com>
2019-04-08 17:51:31 +0200

    streamReducers added

---
+++ src/reducers/streamReducers.js
@@ -0,0 +1,23 @@
+import _ from 'lodash';
+import {
+    FETCH_STREAM,
+    FETCH_STREAMS,
+    CREATE_STREAM,
+    EDIT_STREAM,
+    DELETE_STREAM
+} from '../actions/types';
+
+export default (state = {}, action) => {
+    switch(action.type){
+        case FETCH_STREAM:
+            return { ...state, [action.payload.id]:action.payload};
+        case CREATE_STREAM:
+            return { ...state, [action.payload.id]:action.payload};
+        case EDIT_STREAM:
+            return { ...state, [action.payload.id]:action.payload};
+        case DELETE_STREAM:
+            return state;
+        default:
+            return state;
+    }
+}
