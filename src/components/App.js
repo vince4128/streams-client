70cd7425fa9d6e8000da39d382d78148736da664
vince4128 <vrlt.vincent@gmail.com>
2019-04-09 17:37:48 +0200

    params id added and history object added

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {Router, Route} from 'react-router-dom';
+import history from '../history';
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
@@ -10,16 +11,16 @@ import Header from './Header';
 
 const App = () => {
     return <div className="ui container segment">        
-        <BrowserRouter>            
+        <Router history={history}>            
            <div>
             <Header/>
                <Route path="/" exact component={StreamList}></Route>
                <Route path="/streams/new" exact component={StreamCreate}></Route>
-               <Route path="/streams/edit" exact component={StreamEdit}></Route>
-               <Route path="/streams/delete" exact component={StreamDelete}></Route>
+               <Route path="/streams/edit/:id" exact component={StreamEdit}></Route>
+               <Route path="/streams/delete/:id" exact component={StreamDelete}></Route>
                <Route path="/streams/show" exact component={StreamShow}></Route>               
            </div>
-        </BrowserRouter>
+        </Router>
     </div>;
 };
 
