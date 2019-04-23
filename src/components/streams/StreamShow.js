63784350f8ff42139bb2f9f2e0312996d833c8b7
varlet vincent <vrlt.vincent@gmail.com>
2019-04-23 22:35:04 +0200

    video player added

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,10 +1,44 @@
 import React, {Component} from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 class StreamShow extends Component {
+    constructor(props){
+        super(props);
+
+        this.videoRef = React.createRef();
+    }
+
     componentDidMount() {
-        this.props.fetchStream(this.props.match.params.id);
+
+        const {id} = this.props.match.params;
+        this.buildPlayer();
+        
+    }
+
+    componentDidUpdate(){
+        this.buildPlayer();
+    }
+
+    componentWillUnmount(){
+        this.player.destroy();
+    }
+
+    buildPlayer(){
+        if(this.player || !this.props.stream){
+            return;
+        }
+
+        const {id} = this.props.match.params;
+
+        this.props.fetchStream(id);
+        this.player = flv.createPlayer({
+            type:'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
     }
 
     render(){
@@ -15,6 +49,7 @@ class StreamShow extends Component {
         const {title, description} = this.props.stream;
 
         return<div>
+            <video style={{width:'100%'}} controls ref={this.videoRef}/>
             <h1>{this.props.stream.title}</h1>
             <h5>{this.props.stream.description}</h5>
         </div>;
